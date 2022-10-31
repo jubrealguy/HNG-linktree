bfefbf55de1a004fffbe8ad43a3fe0ce0960ade6
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-10-31 17:16:35 +0100

    hover effect on profile image

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import img from './img/Jubreel_Adebayo.jpeg';
 import share from './img/share.png';
+import imgHover from './img/imghover.png'
 
 const Header=()=> {
     return (
@@ -8,7 +9,10 @@ const Header=()=> {
                 <img id="profile_img" className="share" src={share} alt = "share btn" />
                 <p className="tooltipshare">share</p>
             </div>
-            <img id="profile_img" className="header__img" src={img} alt = "my pic" />
+            <div className='profile'>
+                <img id="profile_img" className="header__img" src={img} alt = "my pic" />
+                <img className="profile_img-hover" src={imgHover} alt="" />
+            </div>
             <h1 className="header__text">Adebayo Jubreel</h1>
             <p id="slack" className='slack'>mrRealest</p>
         </header>
