a9b529dfc1d4cd1c53881e0c617de55a9b3520cc
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-11-05 23:47:03 +0100

    switched from browser router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Contact from './components/Contact';
 import Home from './Home';
 
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element = {<Home /> }></Route>
+    <div>
+      <HashRouter basename='/'>
+        <Routes>
+          <Route path="/" element = {<Home /> }></Route>
 
-      <Route exact path="/contact" element = {<Contact />} ></Route>
-    </Routes>
+          <Route exact path="/contact" element = {<Contact />} ></Route>
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
