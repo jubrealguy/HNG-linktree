8f939589451f64f14f967c4da3480e28bf1902d8
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-11-05 23:36:57 +0100

    worked on my json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import Contact from './components/Contact';
 import Home from './Home';
 
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <HashRouter basename='/'>
-        <Routes>
-          <Route path="/" element = {<Home /> }></Route>
+    <Routes>
+      <Route path="/" element = {<Home /> }></Route>
 
-          <Route exact path="/contact" element = {<Contact />} ></Route>
-        </Routes>
-      </HashRouter>
-    </div>
+      <Route exact path="/contact" element = {<Contact />} ></Route>
+    </Routes>
   );
 }
