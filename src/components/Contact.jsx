9e1e579f1f06c71d50802f5bb75677729a7a6efb
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-11-05 20:38:01 +0100

    did some form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,35 @@
-import Footer from "./Footer"
+import Footer from "./Footer";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Contact() {
+    const [userName, setUserName] = useState("");
+    const [LastName, setLastName] = useState("");
+    const [message, setMessage] = useState("");
+    const [email, setemail] = useState("");
+    const [checkBox, setCheckbox] = useState(false);
+    const [error, seterror] = useState(false);
+    const [successMsg, setSuccessMsg] = useState(false);
+    const navigate = useNavigate();
+
+    var pattern =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(userName === "" || LastName === "" || message === "" || checkBox === false || email === "" || email !== email.match(pattern)){
+            seterror(true)
+            console.log(error)
+        }else{
+            setSuccessMsg(true)
+        }
+    };
+    const checkHandler = ()=> {
+        setCheckbox(!checkBox)
+    }
+    const HandleSuccessMsg =()=>{
+        navigate("/")
+    }
+
+
     return (
         <main>
             <section className='contact'>
@@ -10,33 +39,50 @@ function Contact() {
                     <div className="form--name bottom">
                         <div className="form--name-first">
                             <label for="name" className="form--label">First Name</label>
-                            <input type="text" className="form--input-name" id="first_name" placeholder="Enter your first name" required></input>
+                            <input type="text" className="form--input-name form--input" id="first_name" placeholder="Enter your first name" minlength = "3" required onChange={(e) => {
+                            setUserName(e.target.value);}} />
+                            {error && <div className="error-msg">Please enter your First Name</div>}
                         </div>
                         <div className="form--name-last">
                             <label for="name" className="form--label">Last Name</label>
-                            <input type="text" className="form--input-name" id="last_name" placeholder="Enter your last name" required></input>
+                            <input type="text" className="form--input-name form--input" id="last_name" placeholder="Enter your last name" minlength = "3" required onChange={(e) => {
+                            setLastName(e.target.value);}} />
+                            {error && <div className="error-msg">Please enter your Last Name</div>}
                         </div>
                     </div>
 
                     <div className="form--email bottom">
                         <label for="name" className="form--label">Email</label>
-                        <input type="email" className="form--input-email" id="email" placeholder="yourname@email.com" required></input>
+                        <input type="email" className="form--input-email form--input" id="email" placeholder="yourname@email.com" required onChange={(e) => {
+                         setemail(e.target.value);}} />
+                        {error && <div className="error-msg">Please enter your email</div>}
                     </div>
 
                     <div className="form--textarea bottom">
                         <label for="name" className="form--label">Message</label>
-                        <textarea type="text" className="form--input-textarea" id="message" placeholder="Send me a message and I will reply you as soon as possible" required></textarea>
+                        <textarea type="text" className="form--input-textarea form--input" id="message" placeholder="Send me a message and I will reply you as soon as possible..." onChange={(e) => {
+                        setMessage(e.target.value);}} />
+                        {error && <div className="error-msg">please enter message</div>}
                     </div>
 
                     <div className="form--check bottom">
-                        <input type="checkbox" className="check"></input>
-                        <span for="name" className="check-span">You agree to providing data to who may contact you</span>
+                        <input type="checkbox" className="check" checked={checkBox}
+                    onChange={checkHandler} />
+                        <span for="name" className="check-text">You agree to providing data to who may contact you</span>
                     </div>
                     
-                    <button type="submit" className="form--submit" id="submit__btn">Send message</button>
+                    <button type="submit" className="form--submit" id="submit__btn" onClick={handleSubmit}>Send message</button>
                 </form>
             </section>
-            <Footer />
+        <div className="empty"></div>
+        <Footer />
+        {successMsg ? <div className="successMessage">
+            <h4 className="successMessage--text">
+            Thank You!
+            </h4>
+            <p className="successMessage--subtext">Message delivered successfully.</p>
+            <button className="successMessage--btn" onClick={HandleSuccessMsg}>ok</button>
+        </div> : ""}
         </main>
     )
 }
